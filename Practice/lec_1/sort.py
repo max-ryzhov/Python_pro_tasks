58ea15da93a6d8eda681afae247eeb989fcd0c2d
max-ryzhov <max.ryzhov12@gmail.com>
2022-12-08 15:16:22 +0300

    try to copy in new branch

--- Practice/lec_1/sort.py
+++ Practice/lec_1/sort.py
@@ -1,7 +1,6 @@
 a = [0, 7, 23, 18, 2, 5, -5, 0, 2, 11, 3, 7, 1]
-b = []
 
-
+# 1 способ, без пустого списка
 check_border = len(a)
 while check_border > 0:
     check_part = a[:check_border]
@@ -11,14 +10,15 @@ while check_border > 0:
     check_border -= 1
 print(a)
 
-
+# 2 способ, с пустым списком
+b = []
 while len(a) > 0:
     value_min = min(a)
     b.append(value_min)
     a.remove(value_min)
 print(b)
 
-
+# 3 способ, с пустым списком
 while len(a) > 0:
     value_min = a.pop(a.index(min(a)))
     b.append(value_min)
